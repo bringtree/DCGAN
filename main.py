b5f01ea5535eb2a346a393dfcde0c3d8bb3829c8
peisong <bringtree@qq.com>
2017-09-24 00:19:50 +0800

    finish train module

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import tensorflow as tf
 import DCGAN_model as DCGAN
+import file_operas as file_operas
+import time
+import numpy as np
 
-random_number = tf.placeholder(tf.float32, [64, 100])
-real_image_input = tf.placeholder(tf.float32, [64, 64, 64, 3])
+# build model
+model_random_number = tf.placeholder(tf.float32, [64, 100])
+model_real_image_input = tf.placeholder(tf.float32, [64, 64, 64, 3])
 
-fake_image = DCGAN.generator_model(random_number)
-real_sigmoid = DCGAN.discriminator(real_image_input)
+fake_image = DCGAN.generator_model(model_random_number)
+real_sigmoid = DCGAN.discriminator(model_real_image_input)
 fake_sigmoid = DCGAN.discriminator(fake_image, True)
 
 real_discriminator = tf.reduce_mean(
@@ -25,7 +29,35 @@ discriminator_var = []
 generator_var = []
 for var in all_vars:
   if 'generator' in var.name:
-    discriminator_var.append(var)
-  if 'discriminator' in var.name:
     generator_var.append(var)
+  if 'discriminator' in var.name:
+    discriminator_var.append(var)
 
+discriminator_optimizer = tf.train.AdamOptimizer(0.0002).minimize(d_loss, var_list=discriminator_var)
+generator_optimizer = tf.train.AdamOptimizer(0.0002).minimize(g_loss, var_list=generator_var)
+
+with tf.Session() as sess:
+  sess.run(tf.global_variables_initializer())
+  data = file_operas.load_images_src('./data/new_p/')
+  batch_size = len(data) // 64
+
+  for epoch in range(50):
+    for batch_index in range(0, batch_size):
+      batch_image = np.array([
+        file_operas.get_image(image) for image in data[batch_index * 64:(batch_index + 1) * 64]
+      ]).astype(np.float32)
+
+      random_number = np.random.uniform(-1, 1, size=(64, 100)).astype(np.float32)
+
+      sess.run(discriminator_optimizer,
+               feed_dict={model_real_image_input: batch_image, model_random_number: random_number})
+      sess.run(generator_optimizer,
+               feed_dict={model_random_number: random_number})
+      sess.run(generator_optimizer,
+               feed_dict={model_random_number: random_number})
+
+      d = d_loss.eval({model_real_image_input: batch_image, model_random_number: random_number})
+      g = g_loss.eval({model_random_number: random_number})
+      print(time.strftime("%m-%d %H:%M:%S", time.localtime()))
+      print(d)
+      print(g)
