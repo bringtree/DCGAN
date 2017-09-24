8dc834abb80c1959fd4175808cf01111c5260481
peisong <bringtree@qq.com>
2017-09-24 11:51:19 +0800

    add load_model,save_model and save fake_image. and it is a DCGAN model that can run

--- file_operas.py
+++ file_operas.py
@@ -1,6 +1,6 @@
 from glob import glob
 import os
-import numpy
+import numpy as np
 
 from scipy import misc
 
@@ -25,7 +25,7 @@ def get_image(src):
   :return: return a image, shape[64,64,3] between [-1,1]
   """
 
-  return misc.imread(src).astype(numpy.float32) / 127.5 - 1
+  return misc.imread(src).astype(np.float32) / 127.5 - 1
 
 
 def resize(input_src, output_src):
@@ -42,3 +42,22 @@ def resize(input_src, output_src):
     if (os.path.isdir(output_src) == False):
       os.mkdir(output_src)
     Image.open(file).resize((int(64), int(64))).save(output_src + input_file_name_pre + '.jpg')
+
+
+def save_images(src, data):
+  """
+
+  :param src: like ./test.jpg
+  :param data: shape(64,64,64,3)
+  :return:
+  """
+  img = np.zeros((64 * 8, 64 * 8, 3))
+
+  for idx, image in enumerate(data):
+    i = idx % 8
+    j = idx // 8
+    img[j * 64:j * 64 + 64, i * 64:i * 64 + 64] = image
+
+  misc.imsave(src, img)
+
+# image = np.squeeze(merge(data, 64))
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import DCGAN_model as DCGAN
 import file_operas as file_operas
 import time
 import numpy as np
+import re
+import os
 
 # build model
 model_random_number = tf.placeholder(tf.float32, [64, 100])
@@ -35,13 +37,22 @@ for var in all_vars:
 
 discriminator_optimizer = tf.train.AdamOptimizer(0.0002).minimize(d_loss, var_list=discriminator_var)
 generator_optimizer = tf.train.AdamOptimizer(0.0002).minimize(g_loss, var_list=generator_var)
-
+saver = tf.train.Saver()
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
   data = file_operas.load_images_src('./data/new_p/')
   batch_size = len(data) // 64
 
-  for epoch in range(50):
+  ckpt = tf.train.get_checkpoint_state('./checkpoint/')
+  if ckpt and ckpt.model_checkpoint_path:
+    start_epoch = int(re.search('(?<=DCGAN.ckpt-)\d+', ckpt.model_checkpoint_path).group())
+    saver.restore(sess, os.path.join(ckpt.model_checkpoint_path))
+    print('loading suceess')
+  else:
+    start_epoch = 0
+    print('loading fail')
+  for epoch in range(start_epoch + 1, 50):
+
     for batch_index in range(0, batch_size):
       batch_image = np.array([
         file_operas.get_image(image) for image in data[batch_index * 64:(batch_index + 1) * 64]
@@ -57,8 +68,14 @@ with tf.Session() as sess:
                feed_dict={model_random_number: random_number})
 
       print(time.strftime("%m-%d %H:%M:%S", time.localtime()))
+
       if (batch_index % 100 == 0):
         d = d_loss.eval({model_real_image_input: batch_image, model_random_number: random_number})
         g = g_loss.eval({model_random_number: random_number})
-        print('d_loss:' + d, 'g_loss:' + g)
+        print('d_loss:' + str(d), 'g_loss:' + str(g))
 
+        save_image_data = sess.run(fake_image, feed_dict={model_random_number: random_number})
+        file_operas.save_images('./fake_image/' + 'epoch_' + str(epoch) + 'batch_index_' + str(batch_index) + '.jpg',
+                                save_image_data)
+
+    saver.save(sess, './checkpoint/DCGAN.ckpt', global_step=epoch)
